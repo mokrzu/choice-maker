83e21859ecd2b482a02f31ec284776f6f0c37596
Lucas Mokrzycki <mokrzu@gmail.com>
2012-12-11 17:08:42 +0100

    added Choice model

--- moderator/models.py
+++ moderator/models.py
@@ -2,9 +2,14 @@ from django.db import models
 
 # Create your models here.
 class OptionSet(models.Model):
-	name = models.CharField(max_length=30)
+	name = models.CharField(max_length=40)
 	limit = models.IntegerField()
 
 class Option(models.Model):
-	name = models.CharField(max_length=30)
+	name = models.CharField(max_length=40)
 	option_set = models.ForeignKey('OptionSet')
+
+class Choice(models.Model):
+	author = model.CharField(max_length=30)
+	magic_word = model.CharField(max_length=30)
+	option = models.OneToOneField('Option')
